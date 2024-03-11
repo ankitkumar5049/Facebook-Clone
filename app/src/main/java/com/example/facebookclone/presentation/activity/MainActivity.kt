0dc775a4be1b2f748443e81b15c00f47120ea282
Ankit Kumar <ankit@noww.com>
2024-03-11 19:29:12 +0530

    bug fixes on login screen

--- app/src/main/java/com/example/facebookclone/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/facebookclone/presentation/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.facebookclone.R
 import com.example.facebookclone.databinding.ActivityMainBinding
+import com.example.facebookclone.presentation.fragment.SignInFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +14,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding=ActivityMainBinding.inflate(layoutInflater)
 
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val fragment = SignInFragment()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
 
     }
 }
