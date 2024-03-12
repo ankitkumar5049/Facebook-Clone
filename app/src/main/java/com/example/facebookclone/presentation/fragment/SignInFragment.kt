105702f47b960acff91775a3f30bf0597865a3f2
Ankit Kumar <ankit@noww.com>
2024-03-13 00:47:36 +0530

    firebase dependencies added

--- app/src/main/java/com/example/facebookclone/presentation/fragment/SignInFragment.kt
+++ app/src/main/java/com/example/facebookclone/presentation/fragment/SignInFragment.kt
@@ -1,6 +1,7 @@
 package com.example.facebookclone.presentation.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +16,7 @@ class SignInFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
-        Toast.makeText(context, "Otp sent", Toast.LENGTH_SHORT).show()
+
     }
 
 }
