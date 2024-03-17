a6f5dc0985b6e0a2b347769ccd8f24f0f572f2d0
Ankit Kumar <ankit@noww.com>
2024-03-17 14:13:24 +0530

    Firebase SignUp Done

--- app/src/main/java/com/example/facebookclone/presentation/fragment/SignUpFragment.kt
+++ app/src/main/java/com/example/facebookclone/presentation/fragment/SignUpFragment.kt
@@ -1,19 +1,23 @@
 package com.example.facebookclone.presentation.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.facebookclone.R
 import com.example.facebookclone.databinding.FragmentSignUpBinding
 import com.example.facebookclone.presentation.base.BaseFragment
 import com.example.facebookclone.util.GeneralUtils
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding::inflate) {
 
+    private lateinit var auth: FirebaseAuth
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
         initAction()
     }
 
@@ -23,7 +27,43 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding
                 GeneralUtils.showDatePickerDialog(requireContext(),tvDob)
             }
 
+            btnSignUp.setOnClickListener {
+                if(validate()) {
+                    userAuth()
+                }
+            }
+
         }
+    }
+
+
+    private fun validate(): Boolean{
+        if(binding.edtEmail.text.toString().isEmpty()){
+            showToast("Enter email address")
+            return false
+        }
+        else if(binding.edtPassword.text.toString().isEmpty() || binding.edtPassword.text.toString()!=binding.edtConfirmPassword.text.toString()){
+            showToast("Enter valid password")
+            return false
+        }
+        return true
+    }
+
+    private fun userAuth(){
+        auth.createUserWithEmailAndPassword(binding.edtEmail.text.toString(), binding.tvPassword.text.toString())
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    showLog("createUserWithEmail:success")
+                    findNavController().navigate(R.id.action_signUpFragment_to_newsFeedFragment)
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    showLog(binding.edtEmail.toString())
+                    showLog("createUserWithEmail:failure")
+                    showToast("Authentication failed.")
+                }
+            }
     }
 
 }
